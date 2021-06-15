9d903ea11525c492b37631d68db679ccaebee50d
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-15 11:50:17 -0600

    Finish I3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,4 +10,23 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    @activities.sum do |activity|
+      activity.total_cost
+    end
+  end
+
+  def breakout
+    breakout_hash = Hash.new(0)
+    array_of_hashes = @activities.map do |activity|
+      activity.owed
+    end
+    breakout = array_of_hashes.flat_map do |hash|
+      hash.each_with_object(breakout_hash) do |(person, cost), hash|
+        hash[person] += cost
+      end
+    end
+    breakout[0]
+  end
 end
--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Reunion do
   before :each do
     @reunion = Reunion.new("1406 BE")
     @activity_1 = Activity.new("Brunch")
+    @activity_2 = Activity.new("Drinks")
   end
   describe 'instantiation' do
     it 'exists' do
@@ -23,6 +24,37 @@ RSpec.describe Reunion do
       @reunion.add_activity(@activity_1)
 
       expect(@reunion.activities).to eq([@activity_1])
+    end
+
+    it 'can find reunion total cost' do
+      @activity_1.add_participant("Maria", 20)
+      @activity_1.add_participant("Luther", 40)
+      @reunion.add_activity(@activity_1)
+
+      expect(@reunion.total_cost).to eq(60)
+
+      @activity_2.add_participant("Maria", 60)
+      @activity_2.add_participant("Luther", 60)
+      @activity_2.add_participant("Louis", 0)
+      @reunion.add_activity(@activity_2)
+
+      expect(@reunion.total_cost).to eq(180)
+    end
+
+    it 'can find breakout' do
+      @activity_1.add_participant("Maria", 20)
+      @activity_1.add_participant("Luther", 40)
+      @reunion.add_activity(@activity_1)
+      @activity_2.add_participant("Maria", 60)
+      @activity_2.add_participant("Luther", 60)
+      @activity_2.add_participant("Louis", 0)
+      @reunion.add_activity(@activity_2)
+
+      expect(@reunion.breakout).to eq({
+        "Maria" => -10,
+        "Luther" => -30,
+        "Louis" => 40
+        })
     end
   end
 end
